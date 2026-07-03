cda660a6727d6e67c6261a0b4f65124e1be835f8
usefulmove <duane.edmonds@gmail.com>
2026-07-03 05:44:10 -0700

    ...

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ console = Console(theme=md_theme)
 def main():
     # check for file argument
     if len(sys.argv) < 2:
-        console.print("[red]error[/]: no file argument")
+        console.print("[#f15f49]error[/]: no file argument")
+        console.print("usage: [#0080ff]mp <file>[/]")
         sys.exit(1)
     filename = sys.argv[1]
 
@@ -17,7 +18,7 @@ def main():
         with open(filename) as file:
             contents = file.read()
     except FileNotFoundError:
-        console.print(f"[red]error[/]: [#6272a4]{filename}[/] not found")
+        console.print(f"[#f15f49]error[/]: file [#6272a4]{filename}[/] not found")
         sys.exit(1)
 
     # display as rendered markdown in console pager
