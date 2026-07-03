a306947cf0158c345bb4b6c4a2f1019e2736b4c2
usefulmove <duane.edmonds@gmail.com>
2026-07-03 02:52:14 -0700

    0.0.2: general clean up. mvp.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from dracula_theme import dracula_theme, DRACULA_COMMENT, DRACULA_RED
+from dracula_theme import dracula_theme
 from rich.markdown import Markdown
 from rich.console import Console
 import sys
@@ -8,7 +8,7 @@ console = Console(theme=dracula_theme)
 def main():
     # check for file argument
     if len(sys.argv) < 2:
-        console.print(f"[{DRACULA_RED}]error[/]: no file argument")
+        console.print("[red]error[/]: no file argument")
         sys.exit(1)
     filename = sys.argv[1]
 
@@ -17,7 +17,7 @@ def main():
         with open(filename) as file:
             contents = file.read()
     except FileNotFoundError:
-        console.print(f"[{DRACULA_RED}]error[/]: [{DRACULA_COMMENT}]{filename}[/] not found")
+        console.print(f"[red]error[/]: [#6272a4]{filename}[/] not found")
         sys.exit(1)
 
     # display as rendered markdown in console pager
