e1ebbecf596d0bfd0dbb1919f5d72655e23ff4d3
usefulmove <duane.edmonds@gmail.com>
2026-07-03 08:40:28 -0700

    ...

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ def main():
         console.print("[#f15f49]error[/]: no file argument")
         console.print("usage: [#6272a4]mp [#0080ff]<file>[/][/]")
         sys.exit(1)
-    filename = sys.argv[1]
+    arg = sys.argv[1]
 
     # handle help args
-    if filename in ["--help", "-h"]:
-        console.print("[#0080ff]md[/]. [#6272a4]a simple markdown viewer[/].")
+    if arg in ["--help", "-h"]:
+        console.print("[#0080ff]md[/]. [#6272a4 bold]a simple markdown viewer[/].")
         console.print("usage: [#6272a4]mp <file>[/]")
         sys.exit(0)
 
     # read file contents
     try:
-        with open(filename) as file:
+        with open(arg) as file:
             contents = file.read()
     except FileNotFoundError:
-        console.print(f"[#f15f49]error[/]: file [#6272a4]{filename}[/] not found")
+        console.print(f"[#f15f49]error[/]: file [#6272a4 bold]{arg}[/] not found")
         sys.exit(1)
 
     # display as rendered markdown in console pager
