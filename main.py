f30da8ac2612585598a0d2c9533676e2a365add6
usefulmove <duane.edmonds@gmail.com>
2026-07-03 18:50:51 -0700

    0.1.0: added code flags

--- main.py
+++ main.py
@@ -9,27 +9,45 @@ def main():
     # check for file argument
     if len(sys.argv) < 2:
         console.print("[#f15f49]error[/]: no file argument")
-        console.print("usage: [#6272a4]mp [#0080ff]<file>[/][/]")
+        console.print("usage: [#6272a4]mp [#0080ff]<file.md>[/][/]")
         sys.exit(1)
     arg = sys.argv[1]
 
-    # handle help args
+    # handle help flags
     if arg in ["--help", "-h"]:
         console.print("[#0080ff]md[/]. [#6272a4 bold]a simple markdown viewer[/].")
-        console.print("usage: [#6272a4]mp <file>[/]")
+        console.print("usage: [#6272a4]mp [#0080ff]<file.md>[/][/]")
+        console.print("usage: [#6272a4]mp --python [#0080ff]<file.py>[/][/]")
         sys.exit(0)
+
+    # handle code flags
+    filename = arg
+    code_flag = ""
+    if arg.startswith("--"): # if code flag is present
+        code_flag = arg[2:]
+        if len(sys.argv) < 3:
+            console.print("[#f15f49]error[/]: no file argument after code flag")
+            console.print("usage: [#6272a4]mp --python [#0080ff]<file.py>[/][/]")
+            sys.exit(1)
+        filename = sys.argv[2]
 
     # read file contents
     try:
-        with open(arg) as file:
+        with open(filename) as file:
             contents = file.read()
     except FileNotFoundError:
-        console.print(f"[#f15f49]error[/]: file [#6272a4 bold]{arg}[/] not found")
+        console.print(f"[#f15f49]error[/]: file [#6272a4 bold]{filename}[/] not found")
         sys.exit(1)
 
     # display as rendered markdown in console pager
+    def wrap_code(code, lang):
+        return f"```{lang}\n\n{code}\n\n```"
+
     with console.pager(styles=True):
-        console.print(Markdown(contents, code_theme="md"))
+        console.print(Markdown(
+            contents if not code_flag else wrap_code(contents, code_flag),
+            code_theme="md"
+        ))
 
 if __name__ == "__main__":
     main()
